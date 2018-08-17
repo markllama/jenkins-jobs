212baa63b7239def98086d910ce9528a19312781
Mark Lamourine (work) <markllama@gmail.com>
2018-08-17 19:49:39 -0400

    add gitattr stuff

--- src/com/redhat/art/GitHubRepository.groovy
+++ src/com/redhat/art/GitHubRepository.groovy
@@ -143,7 +143,7 @@ class GitHubRepository {
 
     def set_attribute(String filepath, String attrname, String attrvalue) {
         // check if the attribute is already set some day
-        def gitattributes = "${path}/.gitattributes"
+        def gitattributes = "${this.path}/.gitattributes"
         //def command = "echo '${filepath} ${attrname}=${attrvalue}' >> ${gitattributes}"
         //pipeline.echo command
     }
