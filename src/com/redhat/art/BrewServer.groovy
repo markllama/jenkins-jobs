150731f180cc3f2a6f05a3f87aef29192da9d777
Mark Lamourine (work) <markllama@gmail.com>
2018-09-07 18:48:33 -0400

    add brew server skeleton

---
+++ src/com/redhat/art/BrewServer.groovy
@@ -0,0 +1,27 @@
+//
+// This library describes a class of objects to manage interactions with the
+// Brew/Koji build system
+//
+
+class BrewServer() {
+    def url
+    def principle
+    def keytab
+    def pipeline
+
+    def BrewServer(Map init) {
+        url = init.url
+        principle = init.principle
+        keytab = init.keytab
+        pipeline = init.pipeline
+    }
+
+    def login() {
+        pipeline.sh "kinit -k -t ${keytab} ${principle}"
+    }
+}
+
+class BrewTask() {
+
+}
+
